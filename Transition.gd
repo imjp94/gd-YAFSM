3b3e6e10c42eb2157fe8d6c7bcadc5be28d634e7
imjp94 <imjp_94@hotmail.com>
2020-09-28 02:32:11 +0800

    Add State struct that compatible to GraphNode to Transition

--- Transition.gd
+++ Transition.gd
@@ -6,18 +6,15 @@ const EXIT_KEY = "_exit"
 export(String) var from
 export(String) var to
 export(Array, Resource) var conditions
-export(Array, Resource) var transitions setget set_transitions
-
-var from_state_dict = {
-	ENTRY_KEY: [],
-	EXIT_KEY: []
+export(Dictionary) var states = {
+	ENTRY_KEY: STATE_STRUCT.duplicate(),
+	EXIT_KEY: STATE_STRUCT.duplicate()
 }
 
 
-func _init(p_from="", p_to="", p_transitions=[]):
+func _init(p_from="", p_to=""):
 	from = p_from
 	to = p_to
-	transitions = p_transitions
 
 func transit(params={}):
 	if not conditions:
@@ -32,27 +29,15 @@ func transit(params={}):
 			else: # Condition without value property is a trigger
 				return to
 
-func update_from_state_dict():
-	for transition in transitions:
-		var pair = from_state_dict.get(transition.from)
-		if transition.from and transition.to:
-			if not pair:
-				pair = []
-				from_state_dict[transition.from] = pair
-		elif transition.from or transition.to:
-			if transition.to:
-				# Entry transition
-				pair = from_state_dict[ENTRY_KEY]
-			else:
-				# Exit transition
-				pair = from_state_dict[EXIT_KEY]
-		elif not (transition.from and transition.to):
-			push_warning("Empty Transition %s" % transition.resource_path)
-			continue
-		pair.append(transition)
+func add_transition(from_state, transition):
+	var state = states.get(from_state)
+	if not state:
+		state = STATE_STRUCT.duplicate()
+		states[from_state] = state
+	state.transitions.append(transition)
 
 func get_current_transitions(current_state):
-	return from_state_dict[current_state]
+	return states[current_state].transitions
 
 func get_entry():
 	return get_entries()[0] # TODO: Should no assume one entry
@@ -61,11 +46,12 @@ func get_exit():
 	return get_exits()[0] # TODO: Should no assume one exit
 
 func get_entries():
-	return from_state_dict[ENTRY_KEY]
+	return states[ENTRY_KEY].transitions
 	
 func get_exits():
-	return from_state_dict[EXIT_KEY]
+	return states[EXIT_KEY].transitions
 
-func set_transitions(arr):
-	transitions = arr
-	update_from_state_dict()
+const STATE_STRUCT = {
+	"offset": Vector2.ZERO, # GraphNode.offset 
+	"transitions":[]
+}
