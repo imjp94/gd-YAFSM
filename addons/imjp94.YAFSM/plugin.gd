87e3efd040658982aa3bd1361742e02793ee183b
imjp94 <imjp_94@hotmail.com>
2020-09-28 17:49:27 +0800

    Fix graph editor instance is not assigned  to class member

--- addons/imjp94.YAFSM/plugin.gd
+++ addons/imjp94.YAFSM/plugin.gd
@@ -6,7 +6,7 @@ var graph_editor
 
 
 func _enter_tree():
-	var graph_editor = GraphEditor.instance()
+	graph_editor = GraphEditor.instance()
 	add_control_to_bottom_panel(graph_editor, "YAFSM")
 
 func _exit_tree():
