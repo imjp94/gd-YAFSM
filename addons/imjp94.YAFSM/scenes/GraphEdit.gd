79a9d95324e7ae190e3da7262dc562de1908e934
imjp94 <imjp_94@hotmail.com>
2020-09-30 17:40:34 +0800

    Remove OverlayLog

--- addons/imjp94.YAFSM/scenes/GraphEdit.gd
+++ addons/imjp94.YAFSM/scenes/GraphEdit.gd
@@ -11,7 +11,6 @@ const DEFAULT_NODE_NAME = "State"
 const DEFAULT_NODE_OFFSET = Vector2.ZERO
 
 onready var ContextMenu = $ContextMenu
-onready var OverlayLog = $Overlay/Log
 
 var focused_object setget set_focused_object
 var focused_state_machine setget set_focused_state_machine
@@ -67,7 +66,6 @@ func _on_delete_nodes_request():
 		remove_child(node)
 		focused_state_machine.remove_state(node.name)
 	selected_nodes.clear()
-	check_entry_node()
 
 func _on_node_selected(node):
 	selected_nodes[node.name] = node
@@ -102,7 +100,6 @@ func _on_ContextMenu_index_pressed(index):
 				return
 			var node = EntryGraphNode.instance()
 			add_node(node, State.ENTRY_KEY, local_mouse_pos)
-			OverlayLog.text = ""
 		2: # Add Exit
 			if State.EXIT_KEY in focused_state_machine.states:
 				push_warning("Exit node already exist")
@@ -128,7 +125,6 @@ func _on_focused_state_machine_changed(new_state_machine):
 	if new_state_machine:
 		clear_graph()
 		draw_graph()
-		check_entry_node()
 	else:
 		clear_graph()
 
@@ -175,11 +171,6 @@ func remove_node_connections(node_name):
 	for connection in get_connection_list():
 		if connection.from == node_name or connection.to == node_name:
 			disconnect_state_node(connection.from, connection.from_port, connection.to, connection.to_port)
-
-func check_entry_node():
-	if focused_state_machine:
-		if not focused_state_machine.has_entry():
-			OverlayLog.text = "Warning: Entry node is required, right-click on graph and select Add Entry"
 
 func set_focused_object(obj):
 	if focused_object != obj:
