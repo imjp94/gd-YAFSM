75b7392e57dd32c19ed970aa7f503db1b7ce0408
imjp94 <imjp_94@hotmail.com>
2020-10-03 19:54:43 +0800

    Fix FloatConditionEditor doesn't always show decimals

--- addons/imjp94.YAFSM/scenes/condition_editors/FloatConditionEditor.gd
+++ addons/imjp94.YAFSM/scenes/condition_editors/FloatConditionEditor.gd
@@ -11,7 +11,7 @@ func _ready():
 	FloatValue.connect("focus_exited", self, "_on_FloatValue_focus_exited")
 
 func _on_value_changed(new_value):
-	FloatValue.text = str(stepify(new_value, 0.01))
+	FloatValue.text = str(stepify(new_value, 0.01)).pad_decimals(2)
 
 func _on_FloatValue_text_entered(new_text):
 	change_value_action(_old_value, float(new_text))
@@ -25,4 +25,4 @@ func _on_FloatValue_focus_exited():
 func _on_condition_changed(new_condition):
 	._on_condition_changed(new_condition)
 	if new_condition:
-		FloatValue.text = str(stepify(new_condition.value, 0.01))
+		FloatValue.text = str(stepify(new_condition.value, 0.01)).pad_decimals(2)
