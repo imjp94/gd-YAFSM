62ffb139566853d8a28c90012608047adf6e7c12
imjp94 <imjp_94@hotmail.com>
2020-10-29 19:15:26 +0800

    Implement basic connection and movement

--- FlowChartNode.gd
+++ FlowChartNode.gd
@@ -6,21 +6,11 @@ var stylebox = StyleBoxFlat.new()
 
 func _init():
 	focus_mode = FOCUS_CLICK
+	mouse_filter = MOUSE_FILTER_PASS
 
 func _ready():
 	connect("focus_entered", self, "_on_focused_entered")
 	update()
-
-# func _gui_input(event):
-# 	if event is InputEventMouseButton:
-# 		if event.pressed:
-# 			grab_focus()
-# 			accept_event()
-
-func _process(delta):
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) and get_focus_owner() == self:
-		rect_position = get_global_mouse_position()
-		update()
 
 func _draw():
 	draw_style_box(stylebox, Rect2(Vector2.ZERO, rect_size))
