87b48e423a82e43b619089872f520b24089ecdb2
imjp94 <imjp_94@hotmail.com>
2020-12-29 05:17:25 +0800

    Add StateInspector & Hide State/Transition's variables from inspector

---
+++ addons/imjp94.yafsm/scenes/state_nodes/StateInspector.gd
@@ -0,0 +1,9 @@
+extends EditorInspectorPlugin
+
+const State = preload("res://addons/imjp94.yafsm/src/states/State.gd")
+
+func can_handle(object):
+	return object is State
+
+func parse_property(object, type, path, hint, hint_text, usage):
+	return true
