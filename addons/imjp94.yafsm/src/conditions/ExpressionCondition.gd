b7ad09c4c23f3752664341aead71d1c4e879dea0
Tan Jian Ping <imjp_94@hotmail.com>
2021-11-25 22:54:20 +0800

    Support trigger in expression

--- addons/imjp94.yafsm/src/conditions/ExpressionCondition.gd
+++ addons/imjp94.yafsm/src/conditions/ExpressionCondition.gd
@@ -1,39 +1,40 @@
 extends "Condition.gd"
 
+# Get unique variable: before escape (?!\"|')(?!(and)|(or)|(false)|(true)|\d|\.\b)\b(\w+)\b(?![\"|'])(?!.*\5) 
+const PATTERN_UNIQUE_PARAM = "(?!\\\"|\')(?!(and)|(or)|(false)|(true)|\\d|\\.\\b)\\b(\\w+)\\b(?![\\\"|\'])(?!.*\\5)"
+
 var expression = Expression.new()
+var regex = RegEx.new()
 
-func _ready():
-	pass # Replace with function body.
+var _expression_params = []
 
-func execute(params = {}, local_params = {}):
-	var execute_params = params.duplicate()
-	for local_param_key in local_params.keys():
-		execute_params[local_param_key] = local_params[local_param_key]
+func _init():
+	regex.compile(PATTERN_UNIQUE_PARAM)
+	self.connect("name_changed", self, "_on_name_changed")
+
+func execute(params = {}, local_expression_params = {}):
+	var execute_expression_params = params.duplicate()
+	for local_param_key in local_expression_params.keys():
+		execute_expression_params[local_param_key] = local_expression_params[local_param_key]
+
+	var values = []
+	for param in _expression_params:
+		var value = execute_expression_params.get(param, false)
+		values.append(true if value == null else value) # null == trigger
 	
-	var trigger_variable = TriggerVariable.new(execute_params)
-	
-	var expression_input_names = execute_params.keys()
-	expression_input_names.append("trigger")
-	
-	var expression_input_values = execute_params.values()
-	expression_input_values.append(trigger_variable)
-	
-	var error = expression.parse(name, expression_input_names)
+	var error = expression.parse(name, _expression_params)
 	if error != OK:
 		print(expression.get_error_text())
 		return false
 	
-	var result = expression.execute(expression_input_values, null, true)
+	var result = expression.execute(values, null, true)
 	if not expression.has_execute_failed():
 		return result
 	else:
 		return false
 
-class TriggerVariable:
-	var execute_params
-	
-	func _init(execute_params = {}):
-		self.execute_params = execute_params.duplicate()
-	
-	func _get(property):
-		return property in execute_params
+func _on_name_changed(old, new):
+	var results = regex.search_all(name)
+	if results:
+		for result in results:
+			_expression_params.append(result.get_string())
